04acf3bf76e41ccb0049b6b7574972193908e6f3
admin <admin@pfb07.cshl.edu>
2017-10-17 11:50:44 -0400

    added the number of days I've been in the course.

--- pythonfiles/python.begins.py
+++ pythonfiles/python.begins.py
@@ -12,6 +12,8 @@ my_activity=sys.argv[3]
 
 my_animal=sys.argv[4]
 
+num_days=str(int(sys.argv[5])-15)
+
 print("My name is " + my_name +  ".")#prints my name
 
 print("My favorite color is " + my_color +  ".")#prints my favorite color
@@ -19,3 +21,6 @@ print("My favorite color is " + my_color +  ".")#prints my favorite color
 print("I enjoy " + my_activity +  ".")#prints my favorite activity
 
 print("My animal of choice is the noble " + my_animal +  ".")#prints my favorite animal
+
+print("I have been in this course for " + num_days + " days.") #prints the number of days
+
