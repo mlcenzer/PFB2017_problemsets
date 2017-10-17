c027fdb7b4853821638e25f19994f5323337ce0c
admin <admin@pfb07.cshl.edu>
2017-10-17 11:33:20 -0400

    added file with my name and some preferences

---
+++ pythonfiles/python.begins.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+print("Hello New York!")
+	
+my_name=sys.argv[1]
+
+my_color=sys.argv[2]
+
+my_activity=sys.argv[3]
+
+my_animal=sys.argv[4]
+
+print("My name is " + my_name +  ".")#prints my name
+
+print("My favorite color is " + my_color +  ".")#prints my favorite color
+
+print("I enjoy " + my_activity +  ".")#prints my favorite activity
+
+print("My animal of choice is the noble " + my_animal +  ".")#prints my favorite animal
